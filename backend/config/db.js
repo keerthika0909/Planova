a8d73661da0b811901be0240987757aa24b1f293
Keerthika Badri <badrikeerthika@gmail.com>
2026-06-12 11:03:07 +0530

    fix: use connection pool to prevent server crash

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
   if (err) {
-    console.log("Database Error:", err);
+    console.log("Database Error:", err.message);
   } else {
     console.log("MySQL Connected");
+    conn.release();
   }
 });
 
